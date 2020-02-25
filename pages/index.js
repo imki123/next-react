4b97caa99b7eb357c45383e505ef805b2358cbbc
kiyoung <popping2606@gmail.com>
2020-02-26 01:24:25 +0900

    styling in component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+
 export default function Index() {
     return (
         <div>
-            <p>Hello Next.js</p>
+            <Link href="/about">
+                <a>About Page</a>
+
+            </Link>
+            <p>
+                <span>Hello Next.js</span>
+
+            </p>
+            <style jsx>{`
+                a{
+                    font-size: 2rem;
+                    color: blue;
+                }
+            `}</style>
         </div>
     );
 }
