667f413bb5362a2eeb1d57a8f63751ccdbbeeb00
Kiyoung <popping2606@gmail.com>
2020-03-30 08:37:41 +0900

    making fl

--- components/kakaoFriendsList.js
+++ components/kakaoFriendsList.js
@@ -1,10 +1,16 @@
-export default function KakaoNav() {
+export default function kakaoFriendsList() {
+    const modalRef = React.createRef();
+    const clickProfile = () =>{
+        const modal = modalRef.current
+        modal.style.display='block'
+        modal.childNodes[0].display='block'
+    }
     return (
         <div>
             <ul className="fl_container">
                 <li className="fl_not fl_title">친구</li>
                 <li className="fl_not fl_subtitle">이름검색</li>
-                <li className="fl">
+                <li className="fl" onClick={clickProfile}>
                     <img className="profile" src="/profile.jpg"/>
                     <span className="profile_name">나의 프로필</span></li>
                 <li className="fl_not fl_subtitle">생일인 친구</li>
@@ -18,6 +24,12 @@ export default function KakaoNav() {
                 <li className="fl">친구5</li>
                 <li className="fl">친구6</li>
             </ul>
+
+            <div className="modal" ref={modalRef}>
+                <div className="modal_profile">
+                </div>
+                <div className="modal_myname">나의 프로필</div>
+            </div>
         <style jsx>{`
         .fl_container{
             padding:10px 0px;
@@ -50,6 +62,29 @@ export default function KakaoNav() {
             display:inline-block;
             padding:14px;
             vertical-align:top;
+        }
+        .modal{
+            display:none;
+            position:absolute;
+            top:8px;
+            left:8px;
+            width:450px;
+            height:537px;
+            background-color:rgba(0,0,0,0.7);
+        }
+        .modal_profile{
+            width:80%;
+            height:80%;
+            margin:10%;
+            background-image:url('/profile.jpg');
+            background-size:cover;
+            border-radius:20px;
+        }
+        .modal_myname{
+            position:absolute;
+            color:white;
+            font-size:1.2rem;
+            text-align:center;
         }
         `}</style>
         </div>
